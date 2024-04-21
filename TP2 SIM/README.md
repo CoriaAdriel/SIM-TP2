03db9c95e2e5b9b8a1353e5a62c3659c6b669720
CoriaAdriel <82388@sistemas.frc.utn.edu.ar>
2024-04-21 18:14:58 -0300

    New Proyect

---
+++ TP2 SIM/README.md
@@ -0,0 +1,9 @@
+# SIM-TP2
+Integrantes del Grupo:
+  * 78741 - Franciso Rodriguez
+  * 81165 - Franco Belbruno
+  * 82388 - Adriel Coria
+  * 85206 - Genaro Paredes
+  * 89074 - Juan Francisco Marquez
+  * 93569 - Camila Garetto
+  * 93871 - Alex Paredes
---
+++ TP2 SIM/TP2 SIM/Distribuciones/Exponencial.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace TP2_SIM.Distribuciones
+{
+    internal class ExponencialNegativa
+    {
+        public int CantidadMuestra { get; set; }
+        public int CantidadIntervalos { get; set; }
+        public double Lambda { get; set; }
+        public Chart Grafico { get; set; }
+
+        private List<double> Datos = new List<double>();
+        public DataGridView Grilla { get; set; }
+
+        private Random rnd = new Random();
+
+        public double GenerarAleatorio()
+        {
+            //Genera numeros aleatorios entre 0 y 1 sin llegar a 1
+            return rnd.NextDouble();
+        }
+
+        public void GenerarDistribucion()
+        {
+            //Generamos la distibucion Exponencial Negativa con el metodo de la transformada inversa
+            for (int i = 0; i < CantidadMuestra; i++)
+            {
+                double random = GenerarAleatorio();
+                double variableAleatoria = (-1 / Lambda) * (Math.Log(1 - random));
+                double variableAleatoriaTruncada = Math.Truncate(variableAleatoria * 10000) / 10000;
+
+                Datos.Add(variableAleatoriaTruncada);
+
+            }
+
+            GenerarHistograma();
+
+            //Agregamos la lista de variables generadas al DataGridView para poder visualizarlos
+            CargarGrillaDatos(Datos);
+        }
+        public void CargarGrillaDatos(List<double> Datos)
+        {
+            Grilla.Rows.Clear();
+            Grilla.Columns.Clear();
+            Grilla.Columns.Add("ColumaVariablesGeneradas", "Variables Generadas");
+            for (int i = 0; i < Datos.Count(); i++)
+            {
+                Grilla.Rows.Add(Datos[i]);
+            }
+        }
+
+        public void GenerarHistograma()
+        {
+
+            Grafico.Series.Clear();
+
+            double limiteInferior = 0;
+            double limiteSuperior = 0;
+
+            double valorMaximo = Datos.Max();
+            double valorMinimo = Datos.Min();
+            double precision = 0.0001;
+
+            double amplitud = ((valorMaximo - valorMinimo) / CantidadIntervalos) + precision;
+
+            int frecuenciaObservada = 0;
+            double frecuenciaEsperada = 0;
+            double frecuenciaObservadaAcumulada = 0;
+            double frecuenciaEsperadaAcumulada = 0;
+
+            for (int i = 0; i < CantidadIntervalos; i++)
+            {
+                //Calculamos el primer Intervalo del Histograma y apartir de este generamos el resto
+                if (i == 0)
+                {
+                    limiteInferior = valorMinimo;
+                    limiteSuperior = valorMinimo + amplitud;
+
+                    frecuenciaObservada = DeterminarFrecuenciaObservada(Datos, limiteInferior, limiteSuperior);
+                    frecuenciaObservadaAcumulada = frecuenciaObservada;
+                    frecuenciaEsperada = CalcularFrecuenciaEsperada(limiteInferior, limiteSuperior);
+                    frecuenciaEsperadaAcumulada = frecuenciaEsperada;
+                }
+                else
+                {
+                    limiteInferior = limiteSuperior;
+                    limiteSuperior = limiteSuperior + amplitud;
+
+                    frecuenciaObservada = DeterminarFrecuenciaObservada(Datos, limiteInferior, limiteSuperior);
+                    frecuenciaObservadaAcumulada += frecuenciaObservada;
+                    frecuenciaEsperada = CalcularFrecuenciaEsperada(limiteInferior, limiteSuperior);
+                    frecuenciaEsperadaAcumulada += frecuenciaEsperada;
+                }
+
+                //Generamos las etiquetas para el histograma
+                string etiquetaInferiorIntervalo = (Math.Truncate(limiteInferior * 10000) / 10000).ToString();
+                string etiquetaSuperiorIntervalo = (Math.Truncate(limiteSuperior * 10000) / 10000).ToString();
+                string etiquetaIntervalo = etiquetaInferiorIntervalo + " - " + etiquetaSuperiorIntervalo;
+
+                Series serie = Grafico.Series.Add(etiquetaIntervalo);
+                serie.Points.AddXY("Intervalos", frecuenciaObservada);
+
+            }
+
+        }
+
+        public int DeterminarFrecuenciaObservada(List<double> ListaDatos, double limite_inferior, double limite_superior)
+        {
+
+            int frecuenciaObservada = 0;
+
+            foreach (double valor in ListaDatos)
+            {
+                if (limite_inferior <= valor && valor <= limite_superior)
+                {
+                    frecuenciaObservada++;
+                }
+            }
+
+            return frecuenciaObservada;
+        }
+
+        public double CalcularFrecuenciaEsperada(double limiteInferior, double limiteSuperior)
+        {
+            double frecuenciaEsperada = CalcularProbabilidadEsperada(limiteInferior, limiteSuperior) * CantidadMuestra;
+            return frecuenciaEsperada;
+        }
+
+        public double CalcularProbabilidadEsperada(double limiteInferior, double limiteSuperior)
+        {
+            //Usamos la formula de la acumulada para distibuciones exponencial negativa
+            double ProbabilidadEsperada = ((1 - Math.Exp((-Lambda) * limiteSuperior)) - (1 - Math.Exp((-Lambda) * limiteInferior)));
+
+            return ProbabilidadEsperada;
+        }
+
+    }
+}
